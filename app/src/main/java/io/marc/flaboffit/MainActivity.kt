fc804d9831efda4f2053ae8f7fb51aeffbb89226
marc brout <brout.marc@epitech.eu>
2017-11-13 18:50:26 +0100

    Creating TaskActivity

--- app/src/main/java/io/marc/flaboffit/MainActivity.kt
+++ app/src/main/java/io/marc/flaboffit/MainActivity.kt
@@ -2,11 +2,31 @@ package io.marc.flaboffit
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+    enum class Task(val str: String) {
+        LIFT("Weight Lift"),
+        YOGA("Yoga"),
+        CARDIO("Cardio")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weightLayout.setOnClickListener { v ->
+            taskChosen(Task.LIFT)
+        }
+        yogaLayout.setOnClickListener { v ->
+            taskChosen(Task.YOGA)
+        }
+        cardioLayout.setOnClickListener { v ->
+            taskChosen(Task.CARDIO)
+        }
+    }
+
+    private fun taskChosen(task: Task) {
+        
     }
 }
---
+++ app/src/main/java/io/marc/flaboffit/TaskActivity.kt
@@ -0,0 +1,12 @@
+package io.marc.flaboffit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+}
