adf77e361d5dc9c72eb44e6e9dcc77e62f6a7a28
marc brout <brout.marc@epitech.eu>
2017-11-13 20:17:52 +0100

    Added Second Activity

--- app/src/main/java/io/marc/flaboffit/MainActivity.kt
+++ app/src/main/java/io/marc/flaboffit/MainActivity.kt
@@ -1,14 +1,15 @@
 package io.marc.flaboffit
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
-    enum class Task(val str: String) {
-        LIFT("Weight Lift"),
-        YOGA("Yoga"),
-        CARDIO("Cardio")
+    companion object {
+        val TASK_LIFT = "Weight Lifting"
+        val TASK_YOGA = "Yoga"
+        val TASK_CARDIO = "Cardio"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +17,19 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         weightLayout.setOnClickListener { v ->
-            taskChosen(Task.LIFT)
+            taskChosen(TASK_LIFT)
         }
         yogaLayout.setOnClickListener { v ->
-            taskChosen(Task.YOGA)
+            taskChosen(TASK_YOGA)
         }
         cardioLayout.setOnClickListener { v ->
-            taskChosen(Task.CARDIO)
+            taskChosen(TASK_CARDIO)
         }
     }
 
-    private fun taskChosen(task: Task) {
-        
+    private fun taskChosen(task: String) {
+        val intent = Intent(this, TaskActivity::class.java)
+        intent.putExtra("task", task)
+        startActivity(intent)
     }
 }
